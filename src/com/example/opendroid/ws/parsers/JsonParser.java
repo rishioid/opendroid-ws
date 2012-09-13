7b6a1528925d3b4a7de9005ac0b80a124bb3e5db
Rishi K <rishi.kolvekar@wwindia.com>
2012-09-13 16:51:09 +0530

    directory hierarchy changes

---
+++ src/com/example/opendroid/ws/parsers/JsonParser.java
@@ -0,0 +1,19 @@
+package com.example.opendroid.ws.parsers;
+
+import com.example.opendroid.ws.models.WSModel;
+
+public class JsonParser<T extends WSModel> implements Parser<T> {
+
+	@Override
+	public T getResponseObject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T[] getResponseArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
